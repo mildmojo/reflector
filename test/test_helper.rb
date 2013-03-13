17da40a98964a3eab5496e1b9f3fb6430179f1d0
mildmojo <theaterfire@yahoo.com>
2013-03-13 18:20:54 -0400

    Converts tests to use in-memory DB.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
 end
+
+# Forces all threads to share the same connection. This works on
+# Capybara because it starts the web server in a thread.
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+
+# Fix 'table not found' errors for in-memory SQLite DB.
+load Rails.root.join('db/schema.rb')
